775ea29a2d909897df67042588012f33f5413e9d
matmaster2 <matzarych@gmail.com>
2020-01-14 17:29:52 +0100

    Rebuild of Place class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,15 @@ import lifemanager.money.MoneyByPerson;
 import lifemanager.money.Person;
 import lifemanager.money.Place;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+import static java.util.Arrays.asList;
+
 public class Main {
     public static void main(String[] args) {
+
+        /*
         Person p1 = new Person("Person1");
         Person p2 = new Person("Person2");
 
@@ -21,7 +28,28 @@ public class Main {
         System.out.println(listOfPersonForItem);
         listOfPersonForItem.delPerson(p2);
 
-        System.out.println("-------------------------------------------");
+        System.out.println("-------------------------------------------");*/
+
+
+        Place kebab = new Place("Kebab", new ArrayList<>(asList(new Person("Person 1"), new Person("Person 2"))));
+        kebab.addItem(new Item("Na cienkim", 12.4));
+        kebab.addItem(new Item("tortilla", 20.0));
+        kebab.addItems(new ArrayList<>(asList(new Item("Mleko", 5.6), new Item("Podwojne frytasy", 11.2))));
+
+        System.out.println(kebab);
+        System.out.println("-----");
+        kebab.getItem(0);
+
+        MoneyByPerson money = new MoneyByPerson(kebab.getItem(0));
+        System.out.println("-----------------------------");
+        System.out.println(money);
+        System.out.println("-----------------------------");
+        money.addPerson(kebab.getPerson(0),10.0);
+        money.addPerson(new Person("NjuPerson"),2.4);
+        System.out.println(money);
+
+
+
 
     }
 }
